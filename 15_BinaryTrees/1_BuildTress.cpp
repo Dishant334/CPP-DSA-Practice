a084008f77e9834c524ac960c37294ec744f2dfb
dishant334 <dishantk576@gmail.com>
2025-06-30 19:35:34 +0530

    Binary trees

---
+++ 15_BinaryTrees/1_BuildTress.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;    //for left child
+    Node *right;   //for right child
+ 
+    Node(int data){
+        this->data=data;
+         left=right=NULL;
+    }
+};
+    static int idx = -1;
+    Node * buildTrees(vector<int>nodes){
+      idx++;
+      if(nodes[idx]==-1){
+        return NULL;
+      }
+    Node*currNode=new Node(nodes[idx]);
+    currNode->left=buildTrees(nodes);
+    currNode->right=buildTrees(nodes); 
+
+    return currNode;
+    }
+
+
+int main(){
+    vector<int>nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1};
+    Node *root=buildTrees(nodes);
+
+    cout<<"root ="<<root->data<<endl;
+}
