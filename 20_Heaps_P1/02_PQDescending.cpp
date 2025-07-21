a4e6714a4192d9a8f8b67702c446b7f30de528de
dishant334 <dishantk576@gmail.com>
2025-07-21 13:29:55 +0530

    heaps start

---
+++ 20_Heaps_P1/02_PQDescending.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    priority_queue<int,vector<int>,greater<int>>pq;
+
+    pq.push(5);
+    pq.push(10);
+    pq.push(3);
+
+    while(!pq.empty()){
+        cout<<"top : "<<pq.top()<<endl;
+        pq.pop();
+    }
+    return 0;
+}
