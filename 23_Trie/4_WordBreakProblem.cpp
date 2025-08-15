2261c8113511871114dffec249e3bd623f34a839
dishant334 <dishantk576@gmail.com>
2025-08-15 12:29:33 +0530

    Word break problem

---
+++ 23_Trie/4_WordBreakProblem.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Node {
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord;
+
+    Node() {
+        endOfWord = false;
+    }
+};
+
+class Trie {
+public:
+    Node* root;
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string key) {
+        Node* temp = root;
+        for (int i = 0; i < key.size(); i++) {
+            if (temp->children.count(key[i]) == 0) { // if char not present
+                temp->children[key[i]] = new Node();
+            }
+            temp = temp->children[key[i]];
+        }
+        temp->endOfWord = true;
+    }
+
+    bool search(string key) {
+        Node* temp = root;
+        for (int i = 0; i < key.size(); i++) {
+            if (temp->children.count(key[i]) == 0) {
+                return false; // char not found
+            }
+            temp = temp->children[key[i]];
+        }
+        return temp->endOfWord; // true if end of word
+    }
+};
+bool helper(Trie &trie,string key){
+    if(key.size()==0){
+        return true; 
+    }
+    for(int i=0;i<key.size();i++){
+        string first=key.substr(0,i+1);
+        string second=key.substr(i+1);
+
+        if(trie.search(first) && helper(trie,second)){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool wordBreak(vector<string>dict,string key){
+Trie trie;
+for(int i=0;i<dict.size();i++){
+    trie.insert(dict[i]);
+}
+    return helper(trie,key);
+}
+
+
+int main() {
+    vector<string> dict = {"i","like","sam","samsung","mobile","ice"};
+    cout<<wordBreak(dict,"ilikesamsung")<<endl;
+    }
