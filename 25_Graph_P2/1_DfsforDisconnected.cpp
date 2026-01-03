974ab6fc99d1e158e801576be5e69baa07feb6bd
dishant334 <dishantk576@gmail.com>
2026-01-03 18:42:04 +0530

    added graph 2 part

---
+++ 25_Graph_P2/1_DfsforDisconnected.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<list>
+#include<vector>
+using namespace std;
+class Graph{
+    int V;
+    list<int> *l;
+    public:
+    Graph(int V){ //graph of size V
+        this->V=V;
+        l = new list<int>[V]; //new list of size V
+    }
+   void addEdge(int u,int v){
+      l[u].push_back(v);  //u position pe v add hogya in a list
+      l[v].push_back(u);   //v position pe u add hogya in a list 
+   }
+   void print(){
+   for(int i=0;i<V;i++){
+    list<int>neighbours=l[i];  //its the content of neightbour in vertex
+    cout<<i<<" : ";
+    for(int v:neighbours){
+     cout<<v<<" ";
+    }
+    cout<<endl;
+   }    
+   }
+   void dfsHelper(int u,vector<bool>&vis){
+   /* if(vis[u]==true){
+        return;
+    }*/
+    vis[u]=true;
+    cout<< u<<" ";
+    list<int>neighbours=l[u];
+
+    for(int v:neighbours ){
+        if(!vis[v]){   // this is the base case for it
+            dfsHelper(v,vis);
+        }
+    }
+   }
+   void dfs(){
+    vector<bool>vis(V,false);
+    for(int i=0;i<V;i++){
+        if(!vis[i]){
+          dfsHelper(i,vis);  
+        }
+    }
+   }
+};
+
+int main(){
+ Graph graph(5);
+
+ graph.addEdge(0,1);
+ graph.addEdge(1,2);
+ graph.addEdge(1,3);
+ graph.addEdge(2,3);
+ graph.addEdge(2,4); 
+
+ graph.print();
+vector<bool>vis(7,false);
+ graph.dfs();
+
+    return 0;
+}
---
+++ 25_Graph_P2/2_hasCycleUndirected.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<list>
+#include<vector>
+using namespace std;
+class Graph{
+    int V;
+    list<int> *l;
+    public:
+    Graph(int V){ //graph of size V
+        this->V=V;
+        l = new list<int>[V]; //new list of size V
+    }
+   void addEdge(int u,int v){
+      l[u].push_back(v);  //u position pe v add hogya in a list
+      l[v].push_back(u);   //v position pe u add hogya in a list 
+   }
+   void print(){
+   for(int i=0;i<V;i++){
+    list<int>neighbours=l[i];  //its the content of neightbour in vertex
+    cout<<i<<" : ";
+    for(int v:neighbours){
+     cout<<v<<" ";
+    }
+    cout<<endl;
+   }    
+   }
+   bool hasCycle(int u,int parent,vector<bool>&vis){
+    vis[u]=true;
+    cout<< u<<" ";
+    list<int>neighbours=l[u];
+
+    for(int v:neighbours ){
+        if(!vis[v]){   
+          if( hasCycle(v,u,vis)){ //ek baari true agya to hm upar ko true hi true return krte jare..
+            return true;
+          };
+        }else if(vis[v] && parent !=v){
+            return true;
+        }
+    }
+     return false;
+   }
+};
+
+int main(){
+ Graph graph(5);
+
+ graph.addEdge(0,1);
+ graph.addEdge(1,2);
+ graph.addEdge(1,3);
+ graph.addEdge(2,3);
+ graph.addEdge(2,4); 
+
+ graph.print();
+vector<bool>vis(7,false);
+ cout<<graph.hasCycle(0,-1,vis);
+
+    return 0;
+}
